5c9429adb3c9e0c5d865a688417d04503e25d6a2
Keisuke Ohta <dadadak315@gmail.com>
2017-03-02 11:44:17 +0000

    kadai edit profile totyuu2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
   def show
    @user = User.find(params[:id])
-   if(current_user!=@user)
-    redirect_to:root_path
-   end
+   
   end
   
   def new
