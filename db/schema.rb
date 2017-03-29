ebba9a0460780a85bfb7f5bc1f514252441449f2
Keisuke Ohta <dadadak315@gmail.com>
2017-03-29 12:14:41 +0000

    error fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306080321) do
+ActiveRecord::Schema.define(version: 20170329120833) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
